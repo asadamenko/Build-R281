ef22e9431f9f294396beded0cf8a4e52cc11f609
asadamenko <58948024+asadamenko@users.noreply.github.com>
2026-06-09 17:45:34 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Notion R2x1
+
+Building OpenWrt With Github Actions For Notion R2x1 Router.
+
+## Credits
+- [GitHub Actions](https://github.com/features/actions)
+- [openwrt/openwrt](https://github.com/openwrt/openwrt)
+- [P3TERX/Actions-OpenWrt](https://github.com/P3TERX/Actions-OpenWrt)
+- [SuLingGG/OpenWrt-Rpi](https://github.com/SuLingGG/OpenWrt-Rpi)
+
+## Workflow Used
+- [actions/checkout](https://github.com/actions/checkout)
+- [actions/upload-artifact](https://github.com/actions/upload-artifact)
+- [easimon/maximize-build-space](https://github.com/easimon/maximize-build-space)
+- [mknejp/delete-release-assets](https://github.com/mknejp/delete-release-assets)
+- [ncipollo/release-action](https://github.com/ncipollo/release-action)
+- [robinraju/release-downloader](https://github.com/robinraju/release-downloader)
---
+++ scripts/feeds.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Updating OpenWrt feeds configuration..."
+sed -Ei 's|git.openwrt.org/(feed\|project)|github.com/openwrt|g' ./feeds.conf.default
+
+cat >> ./feeds.conf.default <<EOF
+# Extra feeds
+src-git openwrt_packages https://github.com/kenzok8/openwrt-packages
+src-git small https://github.com/kenzok8/small
+
+# Extra packages
+src-git 3ginfo_lite https://github.com/4IceG/luci-app-3ginfo-lite
+src-git lite_watchdog https://github.com/4IceG/luci-app-lite-watchdog
+src-git my_pkg https://github.com/0x4C334E3438/My_PKG
+src-git modemband https://github.com/4IceG/luci-app-modemband
+src-git modemdefine https://github.com/4IceG/luci-app-modemdefine
+src-git sms_tool_js https://github.com/4IceG/luci-app-sms-tool-js
+src-git xray https://github.com/yichya/luci-app-xray
+src-git v2raya https://github.com/v2rayA/v2raya-openwrt
+src-git zapret https://github.com/remittor/zapret-openwrt;zap1
+EOF
